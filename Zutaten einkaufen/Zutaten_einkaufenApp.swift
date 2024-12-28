bad7c3e39a47aa75c814f0284ff9ae1f395a171a
Michael <28543330+Schello805@users.noreply.github.com>
2024-12-28 20:23:15 +0100

    Initial Commit

---
+++ Zutaten einkaufen/Zutaten_einkaufenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zutaten_einkaufenApp.swift
+//  Zutaten einkaufen
+//
+//  Created by Michael Schellenberger on 28.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Zutaten_einkaufenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zutaten einkaufenTests/Zutaten_einkaufenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zutaten_einkaufenTests.swift
+//  Zutaten einkaufenTests
+//
+//  Created by Michael Schellenberger on 28.12.24.
+//
+
+import Testing
+@testable import Zutaten_einkaufen
+
+struct Zutaten_einkaufenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
